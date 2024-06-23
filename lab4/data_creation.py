0c1a641d75341b65e89612b66aeaea14b633c93e
ivlasiza <157727305+ivlasiza@users.noreply.github.com>
2024-06-23 15:35:20 +0500

    Update data_creation.py

--- lab4/data_creation.py
+++ lab4/data_creation.py
@@ -1,1 +1,8 @@
+import pandas as pd
+from catboost import datasets
 
+all_datasets = datasets.titanic()
+df_train = pd.DataFrame(all_datasets[0])
+df_test = pd.DataFrame(all_datasets[1])
+df_train.to_csv('datasets/titanic-train.csv')
+df_test.to_csv('datasets/titanic-test.csv')
