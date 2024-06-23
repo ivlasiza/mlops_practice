849ed4d33b1a4d8cd6f4c5c1eb9d59c9f1f00e64
ivlasiza <157727305+ivlasiza@users.noreply.github.com>
2024-06-23 12:21:12 +0500

    Update model_testing.py

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -2,17 +2,25 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import pandas as pd
 import pickle
 
+# Загрузка обученной модели из файла
 model = pickle.load(open('models/model.pkl', 'rb'))
+
+# Загрузка тестовых данных из CSV файла
 df_test = pd.read_csv('test/test_result.csv')
+
+# Выделение признаков (температура) и целевой переменной (наличие шума) из тестового набора данных
 X_test = df_test[['temp']]
 y_test = df_test['is_noize']
 
+# Предсказание меток классов для тестового набора данных
 predict = model.predict(X_test)
 
+# Вычисление и вывод метрик модели
 print("Metrics:")
 print(f"Accuracy = {accuracy_score(y_test, predict)}")
 print(f"Precision = {precision_score(y_test, predict)}")
 print(f"Recall = {recall_score(y_test, predict)}")
 print(f"F1-score = {f1_score(y_test, predict)}")
 
-print("****** Pipiline finished ******")
+# Завершение работы пайплайна
+print("****** Pipeline finished ******")
